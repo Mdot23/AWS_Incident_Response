b09e2137a78c852a62df1e3dd0092216dcc356c5
Mdot <36588090+Mdot23@users.noreply.github.com>
2019-05-09 21:56:12 -0500

    Update Lambda_S3_PublicAccessOverwrite.py

--- Lambda_S3_Remove Public Access to S3/Lambda_S3_PublicAccessOverwrite.py
+++ Lambda_S3_Remove Public Access to S3/Lambda_S3_PublicAccessOverwrite.py
@@ -89,48 +89,7 @@ def lambda_handler(event, context):
 
 
 
-"""import boto3
-import json
-import os
-import fileinput
-import configparser
-import logging
-from pprint import pprint 
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-print("Loading Function...")
-
-
-def lambda_handler(event, context):
-    logger.info('event{}'.format(event))
-    s3 = boto3.client('s3')
-    RequestParams = event['requestParameters']['bucketPolicy']
-    Bucketname = event['requestParameters']['bucketName']
-    PolicyEvent = event['requestParameters']['bucketPolicy']['Statement'][0]['Effect'] 
-    PolicyStatus = print("Policy Status is currently " + PolicyEvent)
-    print(PolicyStatus)
-    # Save JSON_Policy to path to this on Linux/Unix based filesystem for Lambda and change current directory to /var/tmp
-    os.chdir("/var/tmp")
-    Dir = os.getcwd()
-    save_path = "/var/task"
-    full_name = os.path.join(save_path,"JSON_Policy.JSON")
-    # Performs the operation of configuring the JSON policy based on the input in the JSON formatted event, change to 'Deny' if 'Allow' in event
-    with open(full_name,"r") as JSONfile:
-        JSONDoc = json.load(JSONfile)
-        JSONDoc = RequestParams 
-        if JSONDoc['Statement'][0]['Effect'] == 'Allow' and JSONDoc['Statement'][0]['Principal'] == '*':
-            JSONDoc['Statement'][0]['Effect'] = 'Deny' 
-            # Convert the convereted JSON > python back into python > JSON 
-            JSONDoc = json.dumps(JSONDoc)
-            ModifyPolicy = s3.put_bucket_policy(Policy= JSONDoc, Bucket='%s' %Bucketname)
-            print('Modifying policy...')
-            print(JSONDoc)
-            return ModifyPolicy
-        else:
-            print('Bucket not policy public')"""
-    
    
         
         
